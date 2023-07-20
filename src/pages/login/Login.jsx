dc431702711698972a46bd6bde25d8eb5b0a1a16
suraj.be <suraj.be@media.net>
2023-07-20 19:28:52 +0530

    Added Login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,44 @@
+import { useState } from 'react'
+
+import styles from './login.module.css'
+
 const Login = () => {
-  return <div>Login</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmitHandler = (evt) => {
+    evt.preventDefault()
+    console.log(email, password)
+  }
+
+  return (
+    <form className={styles['login-form']} onSubmit={onSubmitHandler}>
+      <h2>Login</h2>
+      <label>
+        <span>Email: </span>
+        <input
+          type='email'
+          value={email}
+          onChange={(evt) => {
+            setEmail(evt.target.value)
+          }}
+        />
+      </label>
+
+      <label>
+        <span>Password: </span>
+        <input
+          type='password'
+          value={password}
+          onChange={(evt) => {
+            setPassword(evt.target.value)
+          }}
+        />
+      </label>
+
+      <button className='btn'>Login</button>
+    </form>
+  )
 }
 
 export default Login
