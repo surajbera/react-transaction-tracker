7ba913f39834ae04de0edd4a841a537b84d9c212
suraj.be <suraj.be@media.net>
2023-07-20 19:46:09 +0530

    Added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_apiKey,
+  authDomain: import.meta.env.VITE_APP_authDomain,
+  projectId: import.meta.env.VITE_APP_projectId,
+  storageBucket: import.meta.env.VITE_APP_storageBucket,
+  messagingSenderId: import.meta.env.VITE_APP_messagingSenderId,
+  appId: import.meta.env.VITE_APP_appId,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+/* Initialize Service */
+const projectDb = getFirestore(app)
+const projectAuth = getAuth(app)
+
+export { projectDb, projectAuth }
