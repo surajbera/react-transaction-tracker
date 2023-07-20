73b31bb26e104ed4f51e2391c3d30588d5bea65e
suraj.be <suraj.be@media.net>
2023-07-20 19:14:52 +0530

    Created initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { Home } from './pages'
+import { Login } from './pages'
+import { Signup } from './pages'
+
 import './App.css'
 
 function App() {
-  return <>Helo</>
+  return (
+    <div id='root-inner'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Signup from './signup/Signup'
+import Login from './login/Login'
+import Home from './home/Home'
+
+export { Signup, Login, Home }
