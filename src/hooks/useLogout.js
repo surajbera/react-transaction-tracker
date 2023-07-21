b9205120bafc82c0dda72990f8d7004d94d96872
suraj.be <suraj.be@media.net>
2023-07-21 20:27:17 +0530

    created useLogout hook

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,61 @@
+import { useReducer } from 'react'
+
+import { projectAuth } from '../firebase/config'
+import { signOut } from 'firebase/auth'
+
+import { useConsole } from './useConsole'
+import { useAuthContext } from './useAuthContext'
+
+export const useLogout = () => {
+  const IS_PENDING = 'IS_PENDING'
+  const IS_ERROR = 'IS_ERROR'
+  const { dispatchLogoutEvent } = useAuthContext()
+
+  const logoutReducer = (state, action) => {
+    switch (action.type) {
+      case IS_PENDING:
+        return { ...state, isPending: action.payload }
+      case IS_ERROR:
+        return { ...state, isError: action.payload }
+      default:
+        return state
+    }
+  }
+
+  const initialState = {
+    isPending: false,
+    isError: null,
+  }
+
+  const [state, dispatch] = useReducer(logoutReducer, initialState)
+
+  /* remove this */
+  useConsole('useLogout hook ran')
+
+  const setIsPending = (value) => {
+    dispatch({ type: IS_PENDING, payload: value })
+  }
+
+  const setIsError = (value) => {
+    dispatch({ type: IS_ERROR, payload: value })
+  }
+
+  const logOut = async () => {
+    setIsError(null)
+    setIsPending(true)
+    try {
+      await signOut(projectAuth)
+      dispatchLogoutEvent()
+      setIsPending(false)
+      setIsError(null)
+    } catch (error) {
+      console.log(error.message)
+      setIsPending(false)
+      setIsError(error.message)
+    }
+  }
+
+  const value = { ...state, logOut }
+
+  return value
+}
