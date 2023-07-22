199df7da334d2272996c4588abbf3daa97b80d6d
suraj.be <suraj.be@media.net>
2023-07-22 20:26:19 +0530

    Added a transaction form component and transaction list section on homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,18 @@
+// styles
+import styles from './home.module.css'
+
+// components
+import TransactionForm from './TransactionForm'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  )
 }
 
 export default Home
---
+++ src/pages/home/TransactionForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const TransactionForm = () => {
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState('')
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log({
+      name,
+      amount,
+    })
+  }
+
+  return (
+    <>
+      <h3>Add a Transaction</h3>
+      <form onSubmit={onSubmitHandler}>
+        <label>
+          <span>Transaction name:</span>
+          <input type='text' required onChange={(e) => setName(e.target.value)} value={name} />
+        </label>
+        <label>
+          <span>Amount ($):</span>
+          <input
+            type='number'
+            required
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+          />
+        </label>
+        <button>Add Transaction</button>
+      </form>
+    </>
+  )
+}
+
+export default TransactionForm
