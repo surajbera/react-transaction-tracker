232444c2f563edaff6d7bf26b5a638acf774b0b0
suraj.be <suraj.be@media.net>
2023-07-25 21:49:35 +0530

    deleted unwanted files

--- code-sample.js
+++
@@ -1,9 +0,0 @@
-/* For setting form fields */
-const [formData, setFormData] = useState({ email: '', password: '' })
-
-const onChange = (evt) => {
-  setFormData((prevState) => ({
-    ...prevState,
-    [evt.target.id]: evt.target.value,
-  }))
-}
