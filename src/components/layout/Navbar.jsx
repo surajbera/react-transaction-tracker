ce02e91fb85439ee22ab7a60bd3ef64cfde44968
suraj.be <suraj.be@media.net>
2023-07-26 16:46:28 +0530

    Added some basic styling for header

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 /* libraries */
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useMatch } from 'react-router-dom'
+import classNames from 'classnames'
 
 /* hooks */
 import { useLogout } from '../../hooks'
@@ -9,46 +10,102 @@ import { useAuthContext } from '../../hooks'
 import FullScreenLoader from '../ui/full-screen-loader/FullScreenLoader'
 
 const Navbar = () => {
-  const { logOut, isPending } = useLogout()
-  const { authUser } = useAuthContext()
-  const { pathname } = useLocation()
+  // const { logOut, isPending } = useLogout()
+  // const { authUser } = useAuthContext()
+  // const { pathname } = useLocation()
+
+  const isLoginRoute = useMatch('/login')
+  const isSignupRoute = useMatch('/signup')
 
   const activeRouteStyles = {}
 
-  const onLogoutHandler = async () => {
-    await logOut()
+  // const onLogoutHandler = async () => {
+  //   await logOut()
+  // }
+
+  const isRouteMatched = () => {
+    return false
+  }
+
+  const authLinkCLasses = (isMatched) => {
+    return classNames([
+      'text-gray-800',
+      'dark:text-white',
+      'hover:bg-gray-50',
+      'focus:ring-4',
+      'focus:ring-gray-300',
+      'font-medium',
+      'rounded-lg',
+      'text-sm',
+      'px-4',
+      'lg:px-4',
+      'py-2',
+      'lg:py-2',
+      'ml-3',
+      'dark:hover:bg-gray-700',
+      'focus:outline-none',
+      'dark:focus:ring-gray-800',
+      { 'text-white': isMatched },
+      { 'bg-primary-600': isMatched },
+      { 'hover:bg-primary-700': isMatched },
+      { 'focus:ring-primary-300': isMatched },
+      { 'dark:bg-primary-600': isMatched },
+      { 'dark:hover:bg-primary-700': isMatched },
+      { 'dark:focus:ring-primary-800': isMatched },
+    ])
   }
 
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>TrackIt</Link>
-          </li>
-          {!authUser && (
-            <>
-              <li>
-                <Link to='login' style={pathname === '/login' ? activeRouteStyles : {}}>
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link to='signup' style={pathname === '/signup' ? activeRouteStyles : {}}>
-                  Signup
-                </Link>
-              </li>
-            </>
-          )}
-          {authUser && (
-            <li>
-              <span onClick={onLogoutHandler}>Logout</span>
-            </li>
-          )}
-        </ul>
+    // <>
+    //   <nav>
+    //     <ul>
+    //       <div className='app-logo'>
+    //         <Link to='/'>TrackIt</Link>
+    //       </div>
+    //       {!authUser && (
+    //         <ul>
+    //           <li>
+    //             <Link to='login' style={pathname === '/login' ? activeRouteStyles : {}}>
+    //               Login
+    //             </Link>
+    //           </li>
+    //           <li>
+    //             <Link to='signup' style={pathname === '/signup' ? activeRouteStyles : {}}>
+    //               Signup
+    //             </Link>
+    //           </li>
+    //         </ul>
+    //       )}
+    //       {authUser && (
+    //         <li>
+    //           <span onClick={onLogoutHandler}>Logout</span>
+    //         </li>
+    //       )}
+    //     </ul>
+    //   </nav>
+    //   {isPending && <FullScreenLoader />}
+    // </>
+
+    <header>
+      <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 shadow-sm'>
+        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+          <div className='app-logo'>
+            <Link to='/' className='text-xl font-semibold whitespace-nowrap dark:text-white'>
+              TrackIt
+            </Link>
+          </div>
+
+          <div className='flex items-center lg:order-2'>
+            <Link to='/login' className={authLinkCLasses(isLoginRoute)}>
+              Login
+            </Link>
+            <Link to='/signup' className={authLinkCLasses(isSignupRoute)}>
+              Signup
+            </Link>
+          </div>
+        </div>
       </nav>
-      {isPending && <FullScreenLoader />}
-    </>
+    </header>
   )
 }
 
