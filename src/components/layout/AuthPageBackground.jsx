9fe07fd5b78fbddb6eccb156e897dac36e07bcd3
suraj.be <suraj.be@media.net>
2023-07-27 13:44:53 +0530

    Auth page text line-height changes

--- src/components/layout/AuthPageBackground.jsx
+++ src/components/layout/AuthPageBackground.jsx
@@ -3,11 +3,10 @@ import AuthPageImg from '../../assets/login-page-img/track-it-img-1.jpg';
 const AuthPageBackground = () => {
   return (
     <div className='w-full h-full relative before:w-full before:h-full before:block before:absolute before:bg-gray-700 before:bg-opacity-80'>
-      <p className='absolute top-2/4 -translate-y-2/4 text-white w-full pr-14 pl-20 font-medium text-2xl italic max-w-[700px]'>
-        Stay on top of your finances with TrackIt, the smart solution to monitor, analyze, and track
-        your transactions. Simplify your life, gain insights into your spending, and keep your
-        financial goals on track. Sign up today and take the first step towards better financial
-        management.
+      <p className='absolute top-2/4 -translate-y-2/4 text-white w-full pr-14 pl-20 font-medium text-2xl italic max-w-[700px] leading-relaxed'>
+        TrackIt empowers you to effortlessly monitor, analyze, and track your finances. Gain
+        insights, achieve your financial goals, and simplify your life. Sign up today for smarter
+        financial management.
       </p>
       <img src={AuthPageImg} alt='' className='w-full h-full object-cover' />
     </div>
