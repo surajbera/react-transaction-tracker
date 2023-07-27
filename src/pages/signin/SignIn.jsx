49c5e74b0cd1d3c281eba525bd3cdc994fbf35d7
suraj.be <suraj.be@media.net>
2023-07-27 16:09:45 +0530

    Completed Signin page

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -110,7 +110,7 @@ const SignIn = () => {
             <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
               Don’t have an account yet?{' '}
               <Link
-                to='/signup'
+                to='/sign-up'
                 className='font-medium text-primary-600 hover:underline dark:text-primary-500'
               >
                 Sign up
