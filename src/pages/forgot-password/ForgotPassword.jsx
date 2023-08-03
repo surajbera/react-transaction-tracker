233656ff5ce6639c7eaff54222ef9f42d7f22975
suraj.be <suraj.be@media.net>
2023-08-03 22:05:02 +0530

    updated ForgotPassword.jsx

--- src/pages/forgot-password/ForgotPassword.jsx
+++ src/pages/forgot-password/ForgotPassword.jsx
@@ -60,7 +60,7 @@ const ForgotPassword = () => {
                 onChange={(evt) => setResetEmail(evt.target.value)}
                 className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                 placeholder='name@company.com'
-                required=''
+                required
               />
             </div>
             <button type='submit' className={submitBtnClasses()}>
