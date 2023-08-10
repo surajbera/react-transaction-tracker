c51faaa29e9370b09ff2d30dba4e9ecb6aca0a3e
suraj.be <suraj.be@media.net>
2023-08-11 00:30:43 +0530

    Updated TransactionForm

--- src/components/layout/TransactionForm.jsx
+++ src/components/layout/TransactionForm.jsx
@@ -1,46 +1,73 @@
 /* libraries */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, forwardRef } from 'react';
 /* hooks */
 import { useAddDoc } from '../../hooks';
 
 const TransactionForm = ({ userId, toggleModal, isModalOpen }) => {
-  console.log(isModalOpen);
   const [name, setName] = useState('');
-  const [amount, setAmount] = useState('');
+  const [cost, setCost] = useState('');
+  const [category, setCategory] = useState('');
   const { isPending, isSuccess, addDocument } = useAddDoc();
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-    await addDocument('transactions', { userUid: userId, name, amount });
+    await addDocument('transactions', { userUid: userId, name, cost, category });
+  };
+
+  const CloseModalButton = () => {
+    return (
+      <button
+        type='button'
+        onClick={toggleModal}
+        className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white'
+        data-modal-toggle='updateProductModal'
+      >
+        <svg
+          aria-hidden={!isModalOpen}
+          className='w-5 h-5'
+          fill='currentColor'
+          viewBox='0 0 20 20'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fillRule='evenodd'
+            d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+            clipRule='evenodd'
+          ></path>
+        </svg>
+        <span className='sr-only'>Close modal</span>
+      </button>
+    );
   };
 
   useEffect(() => {
     if (isSuccess) {
       setName('');
-      setAmount('');
+      setCost(0);
+      setCategory('');
     }
   }, [isSuccess]);
 
   return (
     <>
       {/* <h3>Add a Transaction</h3>
-      <form onSubmit={onSubmitHandler}>
-        <label>
-          <span>Transaction name:</span>
-          <input type='text' required onChange={(e) => setName(e.target.value)} value={name} />
-        </label>
-        <label>
-          <span>Amount (₹):</span>
-          <input
-            type='number'
-            required
-            onChange={(e) => setAmount(e.target.value)}
-            value={amount}
-          />
-        </label>
-        {!isPending && <button>Add Transaction</button>}
-        {isPending && <button disabled>Adding Transaction</button>}
-      </form> */}
+  <form onSubmit={onSubmitHandler}>
+    <label>
+      <span>Transaction name:</span>
+      <input type='text' required onChange={(e) => setName(e.target.value)} value={name} />
+    </label>
+    <label>
+      <span>Amount (₹):</span>
+      <input
+        type='number'
+        required
+        onChange={(e) => setAmount(e.target.value)}
+        value={amount}
+      />
+    </label>
+    {!isPending && <button>Add Transaction</button>}
+    {isPending && <button disabled>Adding Transaction</button>}
+  </form> */}
 
       <div
         id='updateProductModal'
@@ -54,27 +81,7 @@ const TransactionForm = ({ userId, toggleModal, isModalOpen }) => {
               <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
                 Add New Expense
               </h3>
-              <button
-                type='button'
-                onClick={toggleModal}
-                className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white'
-                data-modal-toggle='updateProductModal'
-              >
-                <svg
-                  aria-hidden={!isModalOpen}
-                  className='w-5 h-5'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-                    clipRule='evenodd'
-                  ></path>
-                </svg>
-                <span className='sr-only'>Close modal</span>
-              </button>
+              <CloseModalButton />
             </div>
             <form action='#'>
               <div className='grid gap-4 mb-4 sm:grid-cols-2'>
@@ -89,42 +96,59 @@ const TransactionForm = ({ userId, toggleModal, isModalOpen }) => {
                     type='text'
                     name='name'
                     id='name'
-                    // value='iPad Air Gen 5th Wi-Fi'
+                    value={name}
                     className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                    placeholder='Ex. Apple iMac 27&ldquo;'
+                    placeholder='Ex. Snacks'
+                    onChange={(e) => setName(e.target.value)}
                   />
                 </div>
                 <div>
                   <label
-                    htmlFor='brand'
+                    htmlFor='cost'
                     className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                   >
-                    Brand
-                  </label>
-                  <input
-                    type='text'
-                    name='brand'
-                    id='brand'
-                    // value='Google'
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                    placeholder='Ex. Apple'
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor='price'
-                    className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-                  >
-                    Price
+                    Cost(₹)
                   </label>
                   <input
                     type='number'
-                    // value='399'
-                    name='price'
-                    id='price'
+                    name='cost'
+                    id='cost'
+                    value={cost}
                     className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                    placeholder='$299'
+                    placeholder='Ex. 300'
+                    onChange={(e) => setCost(e.target.value)}
                   />
+                </div>
+                <div>
+                  <label
+                    htmlFor='category'
+                    className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                  >
+                    Select an option
+                  </label>
+                  <select
+                    id='category'
+                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                  >
+                    <option value='' disabled>
+                      Choose a category
+                    </option>
+                    <option value='food_beverages'>Food and Beverages</option>
+                    <option value='social_events'>Social Events</option>
+                    <option value='travel_transportation'>Travel and Transportation</option>
+                    <option value='insurance'>Insurance</option>
+                    <option value='utilities'>Utilities</option>
+                    <option value='rent_mortgage'>Rent and Mortgage</option>
+                    <option value='healthcare'>Healthcare</option>
+                    <option value='personal_care'>Personal Care</option>
+                    <option value='shopping'>Shopping</option>
+                    <option value='entertainment'>Entertainment</option>
+                    <option value='investments_savings'>Investments & Savings</option>
+                    <option value='gifts_donations'>Gifts & Donations</option>
+                    <option value='miscellaneous'>Miscellaneous</option>
+                  </select>
                 </div>
               </div>
               <div className='flex items-center space-x-4'>
