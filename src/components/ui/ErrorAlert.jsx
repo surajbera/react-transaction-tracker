2a21e2ac3c0dac4dee79e60972c66d8104ffce82
suraj.be <suraj.be@media.net>
2023-08-10 22:10:38 +0530

    Error modal css fixes

--- src/components/ui/ErrorAlert.jsx
+++ src/components/ui/ErrorAlert.jsx
@@ -1,11 +1,11 @@
 const ErrorAlert = ({ text, closeAlert }) => {
   return (
     <div
-      className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'
+      className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative pr-8 my-6'
       role='alert'
     >
       <span className='block sm:inline'>{text}</span>
-      <span className='absolute top-0 bottom-0 right-0 px-4 py-3'>
+      <span className='absolute top-0 bottom-0 right-2 top-1/2 -translate-y-2/4'>
         <svg
           className='fill-current h-6 w-6 text-red-500'
           role='button'
