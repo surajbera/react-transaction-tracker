434defa5d5491f41c7969aac9fd135d11f9c3595
suraj.be <suraj.be@media.net>
2023-10-19 22:52:55 +0530

    Modified customConsoleLog

--- src/utils/customConsoleLog.js
+++ src/utils/customConsoleLog.js
@@ -1,5 +1,9 @@
-export const customConsoleLog = (text = 'Default Console Log Message', bgColor = '#bbf7d0') => {
-  const logStyle = `color: black; background: ${bgColor};`;
+export const customConsoleLog = (text = 'Default Console Log Message') => {
+  const colorArr = ['#cbd5e1', '#fed7aa', '#fef08a', '#d9f99d', '#a7f3d0', '#bfdbfe', '#e9d5ff'];
+
+  const logStyle = `color: black; background: ${
+    colorArr[Math.floor(Math.random() * colorArr.length)]
+  };`;
   const LOG_CONSOLE = true;
   if (LOG_CONSOLE) {
     console.log(`%c${text}`, logStyle);
